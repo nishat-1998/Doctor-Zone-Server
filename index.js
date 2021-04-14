63b4ea7121010e3126df3ec35e3fb273f5e542ab
Nishat Ahmed <nishatahmed4485@gmail.com>
2021-04-14 10:41:05 +0100

    last

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express()
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+
 app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('doctors'));
+app.use(fileUpload());
+
 
 app.get('/', (req, res) => {
     res.send("hello from db it's  working")
@@ -18,15 +23,28 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const appointmentCollection = client.db("doctorsZone").collection("appointments");
+  const doctorCollection = client.db("doctorsZone").collection("doctors");
 
     app.post('/addAppointment', (req, res) => {
-        const appointment = req.body;
-        appointmentCollection.insertOne(appointment)
-      
-            .then(result => {
-                res.send(result.insertedCount > 0);
+        const date = req.body;
+        const email = req.body.email;
+        doctorCollection.find({ email: email })
+            .toArray((err, doctors) => {
+                const filter = { date: date.date }
+                if (doctors.length === 0) {
+                    filter.email = email;
+                }
+                appointmentCollection.find(filter)
+                    .toArray((err, documents) => {
+                        console.log(email, date.date, doctors, documents)
+                        res.send(documents);
+                    })
+
             })
         })
+
+
+
         app.get('/appointments', (req, res) => {
             appointmentCollection.find({})
                 .toArray((err, documents) => {
@@ -43,9 +61,43 @@ client.connect(err => {
                     res.send(documents);
                 })
             })
-    
+        
 
-    });
+            app.post('/addADoctor', (req, res) => {
+                const file = req.files.file;
+                const name = req.body.name;
+                const email = req.body.email;
+                const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        doctorCollection.insertOne({ name, email, image })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+            app.get('/doctors', (req, res) => {
+                doctorCollection.find({})
+                    .toArray((err, documents) => {
+                        res.send(documents);
+                    })
+            });
+            app.post('/isDoctor', (req, res) => {
+                const email = req.body.email;
+                doctorCollection.find({ email: email })
+                    .toArray((err, doctors) => {
+                        res.send(doctors.length > 0);
+                    })
+            })
+        
+
+    }); 
 
 
     app.listen(port, () => {
